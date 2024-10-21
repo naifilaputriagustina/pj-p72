5100fd72028ea41dcf069cbc967220087bf7bd6c
putryoye7 <putryoye7@gmail.com>
2024-10-21 10:18:02 +0700

    first commit

--- event.sql
+++ event.sql
@@ -24,41 +24,49 @@ USE `event`;
 DROP TABLE IF EXISTS `jenis_lomba`;
 CREATE TABLE IF NOT EXISTS `jenis_lomba` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nama_lomba` varchar(256) DEFAULT NULL,
-  `penyeleggara` varchar(256) DEFAULT NULL,
+  `nama_lomba` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `penyelenggara` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table event.jenis_lomba: ~0 rows (approximately)
+-- Dumping data for table event.jenis_lomba: ~3 rows (approximately)
 DELETE FROM `jenis_lomba`;
+INSERT INTO `jenis_lomba` (`id`, `nama_lomba`, `penyelenggara`) VALUES
+	(1, 'Lomba kelereng', 'SMK syafi\'i akrom'),
+	(2, 'Lomba karung', 'SMK syafi\'i akrom'),
+	(3, 'makan krupuk', 'SMK Syafii Akrom');
 
 -- Dumping structure for table event.pendaftaran
 DROP TABLE IF EXISTS `pendaftaran`;
 CREATE TABLE IF NOT EXISTS `pendaftaran` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_lomba` int DEFAULT NULL,
-  `nama_pendaftar` varchar(256) DEFAULT NULL,
-  `kelas` varchar(256) DEFAULT NULL,
-  `no_hp` varchar(13) DEFAULT NULL,
+  `nama_pendaftar` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0',
+  `kelas` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0',
+  `no_hp` varchar(13) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0',
   `tgl_daftar` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table event.pendaftaran: ~0 rows (approximately)
+-- Dumping data for table event.pendaftaran: ~1 rows (approximately)
 DELETE FROM `pendaftaran`;
+INSERT INTO `pendaftaran` (`id`, `id_lomba`, `nama_pendaftar`, `kelas`, `no_hp`, `tgl_daftar`) VALUES
+	(9, 1, 'bbbbbb', 'xii', '0874636634', '2024-10-04');
 
 -- Dumping structure for table event.user
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `nama` varchar(256) DEFAULT NULL,
-  `username` varchar(256) DEFAULT NULL,
+  `nama` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `username` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `password` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table event.user: ~0 rows (approximately)
 DELETE FROM `user`;
+INSERT INTO `user` (`id`, `nama`, `username`, `password`) VALUES
+	(14, 'putry', 'user2', '$2y$10$xZF7gc09N8ZJ4oYxtI6Ffulkt6ZScSRP40dl79X.D9XTZCEr9NaGS');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
